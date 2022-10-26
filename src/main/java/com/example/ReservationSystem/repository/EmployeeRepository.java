2f30e2fb3541c525c548ab0778981c1aa4e9528e
David Gellen <gellen.david1@gmail.com>
2022-10-26 19:51:59 +0200

    fix: delete reservation

--- src/main/java/com/example/ReservationSystem/repository/EmployeeRepository.java
+++ src/main/java/com/example/ReservationSystem/repository/EmployeeRepository.java
@@ -4,9 +4,13 @@ import com.example.ReservationSystem.domain.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     @Query(value = "SELECT COUNT(*) FROM Employee", nativeQuery = true)
     Integer getTotalAmount();
+
+    List<Employee> findByReservations_Id(Long id);
 
 }
